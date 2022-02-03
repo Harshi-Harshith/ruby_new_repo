65c280cb96f1316742648a11b70d192b252cf1f0
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-03 17:03:50 +0530

    Updated Exercise9

--- Exercise9.rb
+++ Exercise9.rb
@@ -1,5 +1,16 @@
-puts "That\'s it"
-puts 'Printing backslash "\\". This is how we print backslash'
+#puts "That\'s it"
+#puts 'Printing backslash "\\". This is how we print backslash'
 
-inputArray = get.chomp.to_i
+print "Enter the input String: "
+inputString = gets.chomp
+array = []
+inputString.split(" ").each do |i|
+    array << i
+end
+
+output = Hash.new
+
+array.inject(output) { |h,p|  h[p.length] ||= []; h[p.length] << p;h }
+
+p output.sort.to_h
 #Yet to Complete
