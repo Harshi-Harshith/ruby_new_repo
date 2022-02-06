c13205f6519f4ac6c139981fc40f6600b39da4a4
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-07 00:17:31 +0530

    Add Exercise12.rb

---
+++ Exercise12.rb
@@ -0,0 +1,21 @@
+lowercase=0
+uppercase=0
+numerical=0
+special=0
+print "Enter the input String: "
+inputString = gets.chomp
+inputString.each_char {     |letter|    
+    if letter >='a' and letter<='z'
+        lowercase +=1
+    elsif letter >='A' and letter <='Z'
+        uppercase +=1
+    elsif letter == " "
+        special  +=1
+    elsif letter.to_i.to_s == letter
+        numerical +=1
+    else
+        special+=1
+end
+
+}
+print "Lowercase characters: #{lowercase}\nUppercase characters: #{uppercase}\nNumerical characters: #{numerical}\nSpecial characters: #{special}"
