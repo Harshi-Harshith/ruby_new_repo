593b011fe67c2f0604397a6104dbb87f55f0e594
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-07 11:30:37 +0530

    Modified Exercise11.rb

--- Exercise11.rb
+++ Exercise11.rb
@@ -4,28 +4,47 @@ def pascal(lines)
         print "Number of lines must be greater than 0"
         break
     else
-
-
 =end
+=begin
+def pascal(lines)
+  for i in 1..lines
+    for j in 0..lines-i
+      print " "
+    end
+    c=1
+    for k in 1..i
+      print "#{c} "
+      c= c * (i-k)/k
+    end
+    puts 
+  end
+end
+=end
+
+def fact(n)
+  num=1
+  for i in 1..n
+    num*=i
+  end
+  return num
+end
+
+def pascal(n)
+  for r in 0..n
+    yield(fact(n)/(fact(r)*fact(n-r)))
+  end
+end
 
 print "Number of rows you want to print in the Pascal's triangle: "
 lines = gets.chomp.to_i
-pascal(lines) {|row| puts "#{row} "}
-=end
-=begin
-def pascal(n)
-    raise ArgumentError, "must be positive" if n < 1
-    yield ar = [1]
-    (n-1).times do
-      ar.unshift(0).push(0) 
-      yield ar = ar.each_cons(2).map(&:sum)
-    end
+if lines<=0
+  print "Please provide an input"
+else
+  for i in 0..lines
+    pascal(i) { |n| print "#{n} " }
+    puts
   end
-  puts "provide input"
-  line=gets.chomp.to_i
-  pascal(line){|row| puts row.join("").center(0)}.
-=end
-
+end
 =begin
   def sum(array)
   array.reduce(0) { |sum,num| sum+num}
