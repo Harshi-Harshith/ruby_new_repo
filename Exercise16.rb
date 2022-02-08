b924848979cd327952e6deca3ee9d959853b9b0b
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-08 18:15:57 +0530

    Add Exercise16.rb

---
+++ Exercise16.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+print 'Enter the input string: '
+inputString = gets.chomp
+print 'Enter the string which has to be searched in the input string: '
+comword = gets.chomp
+wordslist = inputString.split(' ')
+# print wordslist
+countofthat = wordslist.count(comword) + wordslist.count(comword.capitalize)
+inputString.gsub(comword, "(#{comword})")
+p "#{inputString} Total Occurences = #{countofthat}"
