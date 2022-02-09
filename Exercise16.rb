95419de0c453703bf54bd2ccc3d7417c6584dd4f
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-09 09:07:43 +0530

    Add final answer Exercise16.rb

--- Exercise16.rb
+++ Exercise16.rb
@@ -2,8 +2,9 @@ print "Enter the input string: "
 inputString = gets.chomp
 print "Enter the string which has to be searched in the input string: "
 comword = gets.chomp
-wordslist = inputString.split(' ')
-#print wordslist
-countofthat = wordslist.count(comword)+wordslist.count(comword.capitalize)
-inputString.gsub(comword,"(#{comword})")
-p "#{inputString} Total Occurences = #{countofthat}"
+comcap = comword.capitalize
+comfullcap = comword.upcase
+inputString = inputString.gsub(/#{comword}/, "(#{comword})")
+inputString = inputString.gsub(/#{comcap}/, "(#{comcap})")
+inputString = inputString.gsub(/#{comfullcap}/, "(#{comfullcap})")
+puts "#{inputString} Total Occurences found: #{inputString.count('(')}"
