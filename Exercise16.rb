d1db1a679058c5c8f07b10c42e8465f542e13cfc
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-09 08:54:23 +0530

    Add final answer Exercise16.rb

---
+++ Exercise16.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+print 'Enter the input string: '
+inputString = gets.chomp
+print 'Enter the string which has to be searched in the input string: '
+comword = gets.chomp
+comcap = comword.capitalize
+comfullcap = comword.upcase
+inputString = inputString.gsub(/#{comword}/, "(#{comword})")
+inputString = inputString.gsub(/#{comcap}/, "(#{comcap})")
+inputString = inputString.gsub(/#{comfullcap}/, "(#{comfullcap})")
+puts "#{inputString} Total Occurences found: #{inputString.count('(')}"
