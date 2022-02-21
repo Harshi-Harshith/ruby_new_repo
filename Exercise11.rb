e88eec4c526ce5579d34bfad41c55e427857efc8
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 17:36:38 +0530

    Align Exercise11

--- Exercise11.rb
+++ Exercise11.rb
@@ -1,38 +1,27 @@
-=begin
-def pascal(lines)
-    if lines<=0
-        print "Number of lines must be greater than 0"
-        break
-    else
+# frozen_string_literal: true
 
-
-=end
-
-print "Number of rows you want to print in the Pascal's triangle: "
-lines = gets.chomp.to_i
-pascal(lines) {|row| puts "#{row} "}
-=end
-=begin
-def pascal(n)
-    raise ArgumentError, "must be positive" if n < 1
-    yield ar = [1]
-    (n-1).times do
-      ar.unshift(0).push(0) 
-      yield ar = ar.each_cons(2).map(&:sum)
-    end
+def fact(n)
+  num = 1
+  (1..n).each do |i|
+    num *= i
   end
-  puts "provide input"
-  line=gets.chomp.to_i
-  pascal(line){|row| puts row.join("").center(0)}.
-=end
+  num
+end
 
-=begin
-  def sum(array)
-  array.reduce(0) { |sum,num| sum+num}
-end 
-print sum([5, 10, 20])
-# => 35
-puts
-puts "123"
-p "123"
-=end
+def pascal(n)
+  (0..n).each do |r|
+    yield(fact(n) / (fact(r) * fact(n - r)))
+  end
+end
+
+print "Upto which number you want to print in the Pascal's triangle: "
+lines = gets.chomp.to_i
+
+if lines <= 0
+  print 'Please provide an input'
+else
+  (0..lines).each do |i|
+    pascal(i) { |n| print "#{n} " }
+    puts
+  end
+end
