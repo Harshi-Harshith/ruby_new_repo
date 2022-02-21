c2b874b7ba37d48952d5f898c1148d8c5c13e78f
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:36:03 +0530

    Aligned Exercise20

--- Exercise20.rb
+++ Exercise20.rb
@@ -1,29 +1,28 @@
+# frozen_string_literal: true
+
 class Interest
-    attr_accessor :p, :t, :r
+  attr_accessor :p, :t, :r
 
-    def initialize(principle,tim,r)
-        @p=principle
-        @t=tim
-        @r=r
-        #@SI = Float((principle*tim*@r)/100)
-        #@CI = Float( ( principle * (1+@r).pow(tim) ) )
-        @SI = (principle * tim * (r)).to_f
-        @CI = ((principle * ((1 + @r)**(tim)) ).round(2) - principle).round(3)
-        @diff= (@CI - @SI).round(3)
-        #CI = ( p* (1 + r)^t ), r=r/100
-    end
+  def initialize(principle, tim, r)
+    @p = principle
+    @t = tim
+    @r = r
+    # @SI = Float((principle*tim*@r)/100)
+    # @CI = Float( ( principle * (1+@r).pow(tim) ) )
+    @SI = (principle * tim * r).to_f
+    @CI = ((principle * ((1 + @r)**tim)).round(2) - principle).round(3)
+    @diff = (@CI - @SI).round(3)
+    # CI = ( p* (1 + r)^t ), r=r/100
+  end
 
-    def difference()
-        if @SI>@CI
-            @diff = @SI - @CI
-        end
-    end
+  def difference
+    @diff = @SI - @CI if @SI > @CI
+  end
 
-    def to_s
-        "SI= #{@SI}\nCI= #{@CI}\nInterest difference= #{@diff}"
-    end
-
+  def to_s
+    "SI= #{@SI}\nCI= #{@CI}\nInterest difference= #{@diff}"
+  end
 end
 
-obj = Interest.new(15000,2.5,0.1)
+obj = Interest.new(15_000, 2.5, 0.1)
 puts obj
