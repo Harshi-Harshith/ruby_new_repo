0c1670238cb9c9138b9c9cac3a60ab4c3e852677
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:33:58 +0530

    Aligned Exercise12

--- Exercise12.rb
+++ Exercise12.rb
@@ -1,21 +1,20 @@
-lowercase=0
-uppercase=0
-numerical=0
-special=0
-print "Enter the input String: "
-inputString = gets.chomp
-inputString.each_char {     |letter|    
-    if letter >='a' and letter<='z'
-        lowercase +=1
-    elsif letter >='A' and letter <='Z'
-        uppercase +=1
-    elsif letter == " "
-        special  +=1
-    elsif letter.to_i.to_s == letter
-        numerical +=1
-    else
-        special+=1
-end
+# frozen_string_literal: true
 
-}
+lowercase = 0
+uppercase = 0
+numerical = 0
+special = 0
+print 'Enter the input String: '
+inputString = gets.chomp
+inputString.each_char do |letter|
+  if (letter >= 'a') && (letter <= 'z')
+    lowercase += 1
+  elsif (letter >= 'A') && (letter <= 'Z')
+    uppercase += 1
+  elsif letter.to_i.to_s == letter
+    numerical += 1
+  else
+    special += 1
+  end
+end
 print "Lowercase characters: #{lowercase}\nUppercase characters: #{uppercase}\nNumerical characters: #{numerical}\nSpecial characters: #{special}"
