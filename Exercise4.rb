6987d5c0d7bad8b6603c41c7a5895fccd25494b9
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:00:49 +0530

    Aligned Exercise4

--- Exercise4.rb
+++ Exercise4.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
 class StringPalindrome
-    def checkPalindrom(str)
-        
-        flag=0
-        if str.length==0
-            print "Please provide an input\n0 sized string is always a Palindrome"
-        elsif str.length==1
-            print "Input String is Palindrome"
+  def checkPalindrom(str)
+    flag = 0
+    case str.length
+    when 0
+      print "Please provide an input\n0 sized string is always a Palindrome"
+    when 1
+      print 'Input String is Palindrome'
+    else
+      charArray = str.split('')
+      (0..((charArray.length / 2) - 1)).each do |i|
+        if charArray[i] == charArray[(charArray.length - 1) - i]
+          flag = 0
         else
-            charArray = str.split('')
-            for i in 0..((charArray.length/2)-1)
-                if charArray[i]==charArray[(charArray.length-1)-i]
-                    flag=0
-                else
-                    flag=1
-                    break
-                end
-            end
-            if flag==0
-                print "Input String is palindrome"
-            else
-                print "Input String is not palindrome"
-            end
+          flag = 1
+          break
         end
+      end
+      if flag.zero?
+        print 'Input String is palindrome'
+      else
+        print 'Input String is not palindrome'
+      end
     end
+  end
 end
 
-print "Enter the input String: "
+print 'Enter the input String: '
 str = gets.chomp
-strPalin = StringPalindrome.new()
+strPalin = StringPalindrome.new
 strPalin.checkPalindrom(str)
