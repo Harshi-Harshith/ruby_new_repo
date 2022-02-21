498e3b75473bef580368f9a61ea663b6e675c846
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:00:59 +0530

    Aligned Exercise5

--- Exercise5.rb
+++ Exercise5.rb
@@ -1,35 +1,31 @@
+# frozen_string_literal: true
+
 $account_no
 class Account
-    attr_accessor :name, :account_no, :balance
+  attr_accessor :name, :account_no, :balance
 
-    def initialize(name,balance)
-        @name=name
-        $account_no = $account_no + 1
-        @account_no = $account_no
-        @balance=balance
-    end
+  def initialize(name, balance)
+    @name = name
+    $account_no = $account_no + 1
+    @account_no = $account_no
+    @balance = balance
+  end
 
-    def to_s
-        "name: #{@name}, account_no: #{@account_no}, balance: #{@balance}"
+  # def to_s
+  #   "name: #{@name}, account_no: #{@account_no}, balance: #{@balance}"
+  # end
 
-    def transfer(sender, recipient, money)
-        deposit(recipient,money)
-        withdraw(sender,money)
-    end
-
-    def deposit(recipient,money)
-        recipient.balance = recipient.balance + money
-        puts "\nThe balance of customer #{recipient.name}  with account number #{recipient.account_no} is #{recipient.balance}"
-    end
-
-    def withdraw(sender,money)
-        sender.balance = sender.balance - money
-        puts "\nThe balance of customer #{sender.name}  with account number #{sender.account_no} is #{sender.balance}"
-    end
-
+  def transfer(sender, recipient, money)
+    @send = sender
+    # deposit(recipient,money)
+    # withdraw(sender,money)
+    @rb = recipient.balance + money
+    puts "\nThe balance of customer #{recipient.name}  with account number #{recipient.account_no} is #{@rb}"
+    @sb = sender.balance - money
+    puts "\nThe balance of customer #{sender.name}  with account number #{sender.account_no} is #{sender.balance}"
+  end
 end
 
-customer_one = Account.new("C1",30000)
-customer_two = Account.new("C2",10000)
-customer_one.transfer(customer_one,customer_two,15000)
-
+customer_one = Account.new('C1', 30_000)
+customer_two = Account.new('C2', 10_000)
+customer_one.transfer(customer_one, customer_two, 15_000)
