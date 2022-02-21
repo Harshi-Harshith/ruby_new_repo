be6cd6715346ca898782be849c75921eadb970cc
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:36:13 +0530

    Aligned Exercise21

--- Exercise21.rb
+++ Exercise21.rb
@@ -1,40 +1,42 @@
+# frozen_string_literal: true
+
 class FactorialProgram
-    attr_accessor :input
+  attr_accessor :input
 
-    def initialize(inputnum)
-        @input = inputnum
-        @answer = 1
-        @answer = fact(@input)
+  def initialize(inputnum)
+    @input = inputnum
+    @answer = 1
+    @answer = fact(@input)
+  end
+
+  def fact(k)
+    @temp = 1
+    if k.zero?
+      return 1
+    elsif k.positive?
+      (1..k).each do |i|
+        @temp *= i
+      end
     end
 
-    def fact(k)
-        @temp = 1
-        if k==0
-            return 1
-        elsif k>0
-            for i in 1..k
-                @temp = @temp*i
-            end
-        end
-        return @temp
-    end
+    @temp
+  end
 
-    def to_s
-        if @input<0
-            raise NegativeInput, "Negative number entered"
-        else
-            "#{@answer}"
-        end
+  def to_s
+    if @input.negative?
+      raise NegativeInput, 'Negative number entered'
+    else
+      @answer.to_s
     end
-    
+  end
 end
 
 class NegativeInput < StandardError
 end
 
-print "Enter the number whose factorial you want to find: "
-#input1 = gets.chomp.to_i
-#puts "#{input1.fact}"
+print 'Enter the number whose factorial you want to find: '
+# input1 = gets.chomp.to_i
+# puts "#{input1.fact}"
 num = gets.chomp.to_i
 input1 = FactorialProgram.new(num)
 puts input1
