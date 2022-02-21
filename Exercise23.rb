2fc3caffb8bf6f624e60a684ab303470d2b4ca32
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:37:06 +0530

    Aligned Exercise23

--- Exercise23.rb
+++ Exercise23.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 require 'CSV'
-csvfile = File.open("read.csv","r")
+csvfile = File.open('read.csv', 'r')
 
-employees = Hash.new {|h,k| h[k]=[]}
+employees = Hash.new { |h, k| h[k] = [] }
 
-CSV.foreach("read.csv",headers: true) do |row|
-    employees[:"#{row["Designation"]}"] << "#{row["Name"]} (EmpId: #{row["EmpId"]})"
+CSV.foreach('read.csv', headers: true) do |row|
+  employees[:"#{row['Designation']}"] << "#{row['Name']} (EmpId: #{row['EmpId']})"
 end
 
-#employees.sort.to_h
-employees.sort_by {|h, k| k}.to_h
-#employees.sort.map
+# employees.sort.to_h
+employees.sort_by { |_h, k| k }.to_h
+# employees.sort.map
 
-File.open("23.txt","w") do |i|
-    employees.each do |h,v|
-        if(v.count>1)
-            i.write("#{h}s: #{v}\n")
-        else
-            i.write("#{h}: #{v}\n")
-        end
+File.open('23.txt', 'w') do |i|
+  employees.each do |h, v|
+    if v.count > 1
+      i.write("#{h}s: #{v}\n")
+    else
+      i.write("#{h}: #{v}\n")
     end
+  end
 end
