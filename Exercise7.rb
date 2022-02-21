664dbca5c7dacd2067d5630cfe17c3743d2612e8
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:01:20 +0530

    Aligned Exercise7

--- Exercise7.rb
+++ Exercise7.rb
@@ -1,20 +1,20 @@
-=begin
-inputString = get.chomp
-def to_s
-    inputString.split("").each do |i|
-        i.swapcase
-=end
+# frozen_string_literal: true
+
+# inputString = get.chomp
+# def to_s
+#     inputString.split("").each do |i|
+#         i.swapcase
 
 class Exercise7
-    attr_accessor :inputString
+  attr_accessor :inputString
 
-    def initialize(inputString)
-        @inputString = inputString
-    end
+  def initialize(inputString)
+    @inputString = inputString
+  end
 
-    def to_s
-        "#{@inputString.swapcase}"
-    end
+  def to_s
+    @inputString.swapcase.to_s
+  end
 end
 
 input1 = Exercise7.new(gets.chomp)
