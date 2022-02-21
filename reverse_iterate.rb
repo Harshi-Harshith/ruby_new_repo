83c4360617225ccfcc4aebe2c99627f243c6fdd2
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:35:40 +0530

    Aligned Reverse Iteration

--- reverse_iterate.rb
+++ reverse_iterate.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 def reverseIterate(array)
-    if array.length()<2
-        return array
-    else
-        n=array.length()
-        return reverseIterate(array[(n/2) .. (n-1)]) + reverseIterate(array[0 .. ((n/2)-1)])
-    end
+  if array.length < 2
+    array
+  else
+    n = array.length
+    reverseIterate(array[(n / 2)..(n - 1)]) + reverseIterate(array[0..((n / 2) - 1)])
+  end
 end
 arr = []
-#print "Enter the length of array: "
-len=gets.chomp.to_i
-while len>0
-    #print "Enter the element: "
-    arr << gets.chomp.to_i
-    len-=1
+# print "Enter the length of array: "
+len = gets.chomp.to_i
+while len.positive?
+  # print "Enter the element: "
+  arr << gets.chomp.to_i
+  len -= 1
 end
 
-if arr.length()==0
-    print "Please provide an input"
+if arr.length.zero?
+  print 'Please provide an input'
 else
-    #arr = reverseIterate(arr)
-    #arr.each do |i|
-    #    print "#{i} "
-    #end
+  # arr = reverseIterate(arr)
+  # arr.each do |i|
+  #    print "#{i} "
+  # end
 end
 
-#print "#{reverse(arr)}"
-#arr = arr.reverseIterate{ |i| print "#{i} "}
+# print "#{reverse(arr)}"
+# arr = arr.reverseIterate{ |i| print "#{i} "}
