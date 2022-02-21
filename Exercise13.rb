a67739a0bc4e37f25f4eff0de0259f90a9882694
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-21 16:34:08 +0530

    Aligned Exercise13

--- Exercise13.rb
+++ Exercise13.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 def fact(input)
-    answer = 1
-    if input<=0
-        return "Provide proper input"
-    else
-        for i in 1..input
-            answer = answer*i
-        end
+  answer = 1
+  if input <= 0
+    return 'Provide proper input'
+  else
+    (1..input).each do |i|
+      answer *= i
     end
-    return answer
+  end
+
+  answer
 end
 
-print "Enter the number whose factorial you want to find: "
+print 'Enter the number whose factorial you want to find: '
 input = gets.chomp.to_i
 puts fact(input)
