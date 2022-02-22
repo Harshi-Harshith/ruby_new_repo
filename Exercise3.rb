c5e4bfac79e7af4621b354df93aaaab6a50c4e5a
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-22 17:36:39 +0530

    Final Exercise3

--- Exercise3.rb
+++ Exercise3.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 def fibonacci(fib)
-    term1=1
-    term2=1
-    while term1<=fib
-        yield term1
-        term1,term2=term2,term1+term2
-    end
+  term1 = 1
+  term2 = 1
+  while term1 <= fib
+    yield term1
+    term1, term2 = term2, term1 + term2
+  end
 end
-       
+
 print "Enter the number of fibonacci series' terms you want to print: "
 fib = gets.chomp.to_i
 fibonacci(fib) { |i| puts i }
