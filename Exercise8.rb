9000933a272185071cab397bafba09db8fa74972
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-22 17:37:28 +0530

    Final Exercise8

--- Exercise8.rb
+++ Exercise8.rb
@@ -1,25 +1,28 @@
-def power(inputArray,raise)
-    outputArray = []
-    for i in inputArray
-        raised = 1
-        for j in 1..raise
-            raised = raised * i
-        end
-        outputArray << raised
-    end
-    print outputArray
+# frozen_string_literal: true
+
+def power(inputArray, pow)
+  outputArray = []
+  inputArray.each do |i|
+    # raised = 1
+    # for j in 1..pow
+    #    raised = raised * i
+    # end
+    raised = i**pow
+    outputArray << raised
+  end
+  print outputArray
 end
 
-print "Enter the number of elements you want in the array: "
+print 'Enter the number of elements you want in the array: '
 size = gets.chomp.to_i
 inputArray = []
-while size>0
-    print "\nEnter element to be added to the array: "
-    inputArray << gets.chomp.to_i 
-    size=size-1
+while size.positive?
+  print "\nEnter element to be added to the array: "
+  inputArray << gets.chomp.to_i
+  size -= 1
 end
-print "\nEnter the power to which every number in the array should be raise to: "
-raise = gets.chomp.to_i
-print "\n Input Array is: #{inputArray}"
-print "\n Output Array is: "
-power(inputArray,raise)
+print "\nEnter the power to which every number in the array should be raised to: "
+pow = gets.chomp.to_i
+print "\nInput Array is: #{inputArray}"
+print "\nOutput Array is: "
+power(inputArray, pow)
