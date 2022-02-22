4c5474a88dea34a03fdd17fe1f65e9c7559b4ee7
Harshi-Harshith <bvlssmaniharshith@gmail.com>
2022-02-22 17:40:27 +0530

    Final Sales Tax Problem

--- salestaxproblem.rb
+++ salestaxproblem.rb
@@ -1,35 +1,31 @@
 # frozen_string_literal: true
 
-class SalesTax
-  itemscost = {}
+itemscost = {}
 
-  addItem = 'y'
-  while addItem == 'y'
-    print 'Name of product: '
-    @itemname = gets.chomp
-    print 'Imported? '
-    @imp = gets.chomp
-    print 'Exempted from sales tax? '
-    @exc = gets.chomp
-    print 'Price: '
-    @cost = gets.chomp.to_i
+addItem = 'y'
+while addItem == 'y'
+  print 'Name of product: '
+  @itemname = gets.chomp
+  print 'Imported? '
+  @imp = gets.chomp
+  print 'Exempted from sales tax? '
+  @exc = gets.chomp
+  print 'Price: '
+  @cost = gets.chomp.to_i
 
-    if @imp == 'yes' && @exc == 'yes'
-      @cost += @cost * 0.05
-    elsif @imp == 'yes' && @exc == 'no'
-      @cost += @cost * 0.15
-    elsif @imp == 'no' && @exc == 'yes'
-      @cost += @cost * 0
-    elsif @imp == 'no' && @exc == 'no'
-      @cost += @cost * 0.10
-    end
-    itemscost[@itemname] = @cost
-    print 'Do you want to add more itens to your list(y/n): '
-    addItem = gets.chomp
+  if @imp == 'yes' && @exc == 'yes'
+    @cost += @cost * 0.05
+  elsif @imp == 'yes' && @exc == 'no'
+    @cost += @cost * 0.15
+  elsif @imp == 'no' && @exc == 'yes'
+    @cost += @cost * 0
+  elsif @imp == 'no' && @exc == 'no'
+    @cost += @cost * 0.10
   end
-  p itemscost
-  print 'Total Cost: '
-  print itemscost.inject(0) { |total, individual| total += individual[1] }
+  itemscost[@itemname] = @cost
+  print 'Do you want to add more itens to your list(y/n): '
+  addItem = gets.chomp
 end
-
-obj1 = SalesTax.new
+p itemscost
+print 'Total Cost: '
+print itemscost.inject(0) { |total, individual| total += individual[1] }
